6e957e9113aa8b0230552985203323878c888e7e
alvarocantador <alvaroinckot@gmail.com>
2014-12-24 14:49:38 -0200

    Include source and examples

---
+++ src/log4net.Appender.PushoverAppender/log4net.Appender.PushoverAppender/PushoverAppender.cs
@@ -0,0 +1,29 @@
+﻿using PushoverClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net.Appender
+{
+    public class PushoverAppender : AppenderSkeleton
+    {
+        public string ApplicationName { get; set; }
+        public string ApplicationKey { get; set; }
+        public string UserKey { get; set; }
+
+        public Pushover pclient { get; set; }
+
+        protected override void Append(Core.LoggingEvent loggingEvent)
+        {
+            pclient = new Pushover(ApplicationKey);
+
+            var response = pclient.Push(
+              string.Format("{0} - {1}", loggingEvent.Level.Name, ApplicationName),
+              loggingEvent.RenderedMessage,
+              UserKey
+          );
+        }
+    }
+}
